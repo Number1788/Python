991ce9dceece053fe1dbb0f1a6b09cea5f516f2c
number1788 <number1788@gmail.com>
2015-08-05 22:58:08 +0800

    dict
    Signed-off-by: number1788 <number1788@gmail.com>

---
+++ tutorial/dict/1.py
@@ -0,0 +1,10 @@
+D= {'food': 'Spam', 'quantity': 4, 'color':'pink'}      #Инициализируем словарь
+print(D['food'])                                        #Выводим значение с ключом food
+D['quantity'] +=1                                       #Добавить +1 к значению ключа quantity
+print(D)    
+F={}                                                    #Другой способ создания словаря
+F['name']= 'Bob'
+F['job']='dev'
+F['age']=40
+print(F)
+print(F['name'])
