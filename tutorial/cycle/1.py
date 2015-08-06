daed536a6b3741c2ab197bd88b367836244a2801
number1788 <number1788@gmail.com>
2015-08-06 23:09:43 +0800

    Touple

--- tutorial/cycle/1.py
+++ tutorial/cycle/1.py
@@ -4,3 +4,9 @@ x=4
 while x >0:                 #цикл while
     print('spam!' * x)
     x-=1
+squares = [x ** 2 for x in [1,2,3,4,5]]     #вычисление в списке, x**2 возведение в степень
+print(squares)
+squares = []
+for x in [1,2,3,4,5]:                       #тоже вычисление, но механизмом итераций
+    squares.append(x**2)
+print (squares)
