bd65c0cb0d27693c8fbcae3e1b3ec3ae3fa235ef
number17 <number1788@gmail.com>
2015-08-14 15:02:13 +0700

    150 class

--- tutorial/class/1.py
+++ tutorial/class/1.py
@@ -1,8 +1,14 @@
-class Worker:
-    def __init__(self,name,pay):
-        self.name = name
+class Worker:                               #
+    def __init__(self,name,pay):            #
+        self.name = name                    #
         self.pay = pay
     def lastname(self):
-        return self.name.split()[-1]
+        return self.name.split()[-1]        #
     def giveRaise(self, percent):
-        self.pay *=(1.0+ percent)
+        self.pay *=(1.0+ percent)           #
+bob = Worker('Bob Smith', 50000)            #
+sue = Worker('Sue Jones', 60000)            #
+print(bob.lastname())                       #
+print(sue.lastname())                       #
+sue.giveRaise(0.1)                          #
+print(sue.pay)
