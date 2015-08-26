725bfbc71548e575845db602665d8e769a6be393
number1788 <number1788@gmail.com>
2015-08-26 23:12:21 +0800

    187 sets

--- tutorial/sets/1.py
+++ tutorial/sets/1.py
@@ -17,7 +17,19 @@ z = x.intersection(y)               #Пересечение
 print(z)
 z.add('SPAM')                       #Добавление нового элемента
 print(z)
-z.update(set(['dx','y']))           #Объеденение
+z.update(set(['d','y']))            #Объеденение(?)
 print(z)
-z.remove('dx')                      #Удаление элемента
+z.remove('d')                       #Удаление элемента
 print(z)
+S= set([1,2,3])
+print(S.union([3,4]))               #объеденение
+print(S.issubset(range(-5,0)))      #проверка является ли S подножеством определнного диапозона
+print(S &{1,5})                     #Пересечение литералы
+print({1,5} | S)                    #Объеденение
+print(S - {1,3})                    #Разность
+print(S >{1,3})                     #Надмоножество
+s1 = set()
+print(s1)
+s1.add(1.23)
+print(s1)
+
