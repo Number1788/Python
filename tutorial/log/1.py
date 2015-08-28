eb3191af7b4cd7ace1fc33f6160e04f28d3ce8e5
Number1788 <Number1788@gmail.com>
2015-08-28 14:59:57 +0700

    191 boolean

--- tutorial/log/1.py
+++ tutorial/log/1.py
@@ -4,3 +4,9 @@ X = None                        #Специальный объект None
 print(X)
 L= [None] * 100                 #Инициализация списка сотней объектов None
 print(L)
+print(type(True))
+print(isinstance(True,int))
+print(True == 1)                #То же самое значение
+print(True is 1)                #Но разные значения
+print(True or False)            #то же что 1 или 0
+print(True +4)                  #:)
