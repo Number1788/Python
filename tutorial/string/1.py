2346718ff36764fc2ca63a506b9898076e86a03e
number1788 <number1788@gmail.com>
2015-09-16 00:03:16 +0800

    237 format

--- tutorial/string/1.py
+++ tutorial/string/1.py
@@ -13,3 +13,8 @@ msg=""" aaaaaa
 bbbbbb'''bbbbbbb""bbbbb'bbb
 ccccccccccccc"""            #"""- многострочная строка-"""
 print(msg)
+exclamation = "Ni"
+print("The knights who say %s!" % exclamation)
+print("%d %s %d you" % (1,'spam' , 4))
+print("%s -- %s -- %s" % (42,3.14159,[1,2,3]))
+      
