bb1a7b30d4dd9add3907a2c10c0d0563f159cd1f
Number1788 <Number1788@gmail.com>
2015-10-09 17:33:28 +0700

    273 dict

--- site/cgi-bin/form.py
+++ site/cgi-bin/form.py
@@ -2,10 +2,13 @@
 #!/usr/bin/env python3
 
 import cgi
+import html
 
 form = cgi.FieldStorage()
 text1= form.getfirst('TEXT_1', 'не задано')
-text2= form.getfirst('TEXT_3', 'не задано')
+text2= form.getfirst('TEXT_2', 'не задано')
+text1=html.escape(text1)
+text2=html.escape(text2)
 print("Content-type: text/html\n")
 text="""<!DOCTYPE HTML>
         <html>
